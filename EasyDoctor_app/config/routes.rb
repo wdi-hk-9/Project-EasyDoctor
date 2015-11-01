64ac065a766432b8e894c6db9b510a76270727b7
Bryan Fong <bryan.fch@gmail.com>
2015-11-01 23:58:39 +0800

    added session controller and updated routes

--- EasyDoctor_app/config/routes.rb
+++ EasyDoctor_app/config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
 root "users#index"
 resources :users, only: [:new, :index, :create, :destroy]
 
+get 'login', to: 'sessions#new'
+resources :sessions, only: [:new, :create, :destroy]
+
 end
