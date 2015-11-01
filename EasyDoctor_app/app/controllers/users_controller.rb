b6971ec925c2a69f77e4ce36941d77322abe223e
Bryan Fong <bryan.fch@gmail.com>
2015-11-01 19:28:16 +0800

    added new and create methods in user controller. new.html.erb is working.

--- EasyDoctor_app/app/controllers/users_controller.rb
+++ EasyDoctor_app/app/controllers/users_controller.rb
@@ -4,8 +4,19 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render 'new'
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :mobile, :email)
   end
 end
