adac7725eaa4ccb90aefe34206b770905210666c
Bryan Fong <bryan.fch@gmail.com>
2015-11-03 07:32:43 +0800

    added appointment index and delete

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,5 +1,6 @@
 class AppointmentsController < ApplicationController
   def index
+    @appointments = Appointment.all
   end
 
   def new
@@ -9,5 +10,18 @@ class AppointmentsController < ApplicationController
   end
 
   def show
+  end
+
+  def destroy
+    @appointment = Appointment.find(params[:id])
+    # destroy
+    @appointment.destroy
+    # redirect_to
+    redirect_to appointments_path
+  end
+
+  private
+  def appointment_params
+    params.require(:appointment)
   end
 end
