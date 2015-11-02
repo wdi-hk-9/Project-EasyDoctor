7fca3754e22517a9d5318c67a2e160cac308bd0b
Fer Martin <fernandomartinsantana@gmail.com>
2015-11-02 11:05:53 +0800

    consolidated migrations, database for production config, minor indentation fixes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,17 +4,16 @@ class SessionsController < ApplicationController
 
   # Authentication logic
   def create
-  user = User.find_by_email(params[:email])
+    user = User.find_by_email(params[:email])
 
-  # email_found && params[:password] == hashed_password ?
-  if user && user.authenticate(params[:password])
-    session[:user_id] = user.id     #added this line to implement session using cookies
-    redirect_to root_path, notice: "logged in!"
-  else
-   flash.now.alert = "invalid login credentials"
-   render "new"  # sessions#new
-  end
-
+    # email_found && params[:password] == hashed_password ?
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id     #added this line to implement session using cookies
+      redirect_to root_path, notice: "logged in!"
+    else
+     flash.now.alert = "invalid login credentials"
+     render "new"  # sessions#new
+    end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
   end
 
   private
-    def user_params
-      params.require(:user).permit(:name, :mobile, :email, :password, :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :mobile, :email, :password, :password_confirmation)
+  end
 end
