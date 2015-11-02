d0b465621577e8f3359134d3017aeab9b82f11bc
Bryan Fong <bryan.fch@gmail.com>
2015-11-02 21:15:51 +0800

    fixed bug - doctor log in seems working. Need double check

--- app/controllers/doctor_sessions_controller.rb
+++ app/controllers/doctor_sessions_controller.rb
@@ -8,7 +8,7 @@ class DoctorSessionsController < ApplicationController
 
     # email_found && params[:password] == hashed_password ?
     if doctor && doctor.authenticate(params[:password])
-      doctor_session[:doctor_id] = doctor.id     #added this line to implement session using cookies
+      session[:doctor_id] = doctor.id
       redirect_to doctors_path, notice: "logged in!"
     else
      flash.now.alert = "invalid login credentials"
@@ -17,7 +17,7 @@ class DoctorSessionsController < ApplicationController
   end
 
   def destroy
-    doctor_session[:doctor_id] = nil     # add this line to implement session using cookie
+    session[:doctor_id] = nil     # add this line to implement session using cookie
     redirect_to root_url, notice: "logged out!"
   end
 end
