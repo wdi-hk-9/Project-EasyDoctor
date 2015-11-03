5d0104a2fcc86973ea1d6bbd4c0d2359d4bf2c09
Bryan Fong <bryan.fch@gmail.com>
2015-11-03 17:51:53 +0800

    user dashboard initialize

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,5 @@
-$(function(){
+// User Signup Form
+$(document).ready(function(){
   var updateViews = function(e) {
     var is_doctor = $('#user_select').val() == "true";
 
@@ -9,8 +10,26 @@ $(function(){
     }
   }
 
+  updateViews();
+
   $('.doctors').hide();
 
   // $('#user_select').on('change', updateViews);
   $('#user_select').on('change', updateViews);
+})
+
+// User Dashboard
+$(document).ready(function () {
+  $('#make-appointment').show();
+  $('#confirmed-appointments').hide();
+
+  $('#show-appt').on('click', function(){
+    $('#make-appointment').hide('slow');
+    $('#confirmed-appointments').show('slow');
+ });
+
+  $('#add-appt').on('click', function(){
+    $('#confirmed-appointments').hide('slow');
+    $('#make-appointment').show('slow');
+ });
 })
