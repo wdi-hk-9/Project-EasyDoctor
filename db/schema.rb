69cdfe843ba0411a62d3d70fed62f45c7cf90b87
Bryan Fong <bryan.fch@gmail.com>
2015-11-03 17:59:17 +0800

    created appointment table and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151101073954) do
+ActiveRecord::Schema.define(version: 20151103095220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.time     "datetime"
+    t.string   "doctor"
+    t.string   "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
