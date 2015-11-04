0edcd3aca19a5cbb6637e1b367497b3139562ea6
“Ian <ianyhkwok@hotmail.com>
2015-11-05 02:02:08 +0800

    Function Completed

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'user/dashboard', to: 'users#dashboard'
 
   get 'signup', to: 'users#new'
+  get '/users/appointments', to: 'appointments#index'
   resources :users, only: [:index, :create, :show] do
     resources :appointments, only: [:new, :create]
   end
