fa62f758a332d5fed84117022adcde3d6a40d485
“Ian <ianyhkwok@hotmail.com>
2015-11-05 15:12:15 +0800

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root "static_pages#index"
 
-  get 'signup', to: 'users#new'
-
   get '/users/appointments', to: 'appointments#index'
   resources :users, only: [:index, :create] do
     resources :appointments, only: [:new, :create]
@@ -10,10 +8,11 @@ Rails.application.routes.draw do
 
   get 'doctors/appointments', to: "appointments#doctor_index"
   put 'doctors/appointments/:id/confirm', to: "appointments#confirm", as: "doctors_appointment_confirm"
-  put 'doctors/appointments/:id/reject', to: "appointments#reject", as: "doctors_appointment_reject"
+  put 'doctors/appointments/:id/reject',  to: "appointments#reject",  as: "doctors_appointment_reject"
 
-  get 'login', to: 'sessions#new'
-  delete 'logout', to: 'sessions#destroy'
+  get '/signup',    to: 'users#new'
+  get '/login',     to: 'sessions#new'
+  delete '/logout', to: 'sessions#destroy'
   resources :sessions, only: [:create]
 
 end
